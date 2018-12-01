973f5bfa4f902367d25dfc49cf307fa1dd6bd686
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-01 18:37:37 +0000

    Deploy JohnDeeBeeCooper/pizza to github.com/JohnDeeBeeCooper/pizza.git:gh-pages

---
+++ precache-manifest.2e74a749f342f4386eff0b491538b557.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "036b780efe45b747c9ba",
+    "url": "/pizza/static/js/main.036b780e.chunk.js"
+  },
+  {
+    "revision": "b73146cfdf517638987e",
+    "url": "/pizza/static/js/1.b73146cf.chunk.js"
+  },
+  {
+    "revision": "24d64a7390fb6e171c82",
+    "url": "/pizza/static/js/runtime~main.24d64a73.js"
+  },
+  {
+    "revision": "3a37791c4739cdc7bfea08c29df29a10",
+    "url": "/pizza/index.html"
+  }
+];
